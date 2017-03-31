66f5eae412b983b33b307fff485498a43513f30b
felipe <felipebaitelman@gmail.com>
2017-03-30 22:13:24 -0300

    removing seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-usuario = User.create([{usuario:'jperez0', nombre: 'Juan0', apellido: 'Pérez0', twitter: 'jperez19900'}])
