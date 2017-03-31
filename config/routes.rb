f68c89a75448a0d2a6ba0f39bdfae77e4521aa05
felipe <felipebaitelman@gmail.com>
2017-03-30 22:59:22 -0300

    adding seeds and modifying final routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
 
   resources :users
 
-  put 'users', to: 'users#create'
-  post 'users/:id', to: 'users#update'
-  get 'users', to: 'users#index'
-  get 'users/:id', to: 'users#show'
-  delete 'users/:id', to: 'users#destroy'
+  put 'usuario', to: 'users#create'
+  post 'usuario/:id', to: 'users#update'
+  get 'usuario', to: 'users#index'
+  get 'usuario/:id', to: 'users#show'
+  delete 'usuario/:id', to: 'users#destroy'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user = User.create([{usuario:'jperez0', nombre: 'Juan0', apellido: 'Perez0', twitter: 'jptwitter0'}])
+user = User.create([{usuario:'jperez1', nombre: 'Juan1', apellido: 'Perez1', twitter: 'jptwitter1'}])
+user = User.create([{usuario:'jperez2', nombre: 'Juan2', apellido: 'Perez2', twitter: 'jptwitter2'}])
+user = User.create([{usuario:'jperez3', nombre: 'Juan3', apellido: 'Perez3', twitter: 'jptwitter3'}])
+user = User.create([{usuario:'jperez4', nombre: 'Juan4', apellido: 'Perez4', twitter: 'jptwitter4'}])
+user = User.create([{usuario:'jperez5', nombre: 'Juan5', apellido: 'Perez5', twitter: 'jptwitter5'}])
+user = User.create([{usuario:'jperez6', nombre: 'Juan6', apellido: 'Perez6', twitter: 'jptwitter6'}])
+user = User.create([{usuario:'jperez7', nombre: 'Juan7', apellido: 'Perez7', twitter: 'jptwitter7'}])
+user = User.create([{usuario:'jperez8', nombre: 'Juan8', apellido: 'Perez8', twitter: 'jptwitter8'}])
+user = User.create([{usuario:'jperez9', nombre: 'Juan9', apellido: 'Perez9', twitter: 'jptwitter9'}])
